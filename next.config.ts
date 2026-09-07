cf8e5df39e7a9b2338b642e95863eab4589b8449
Marius <p.marius.koder@gmail.com>
2026-09-07 12:41:47 +0200

    Configure supported image qualities

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  images: {
+    qualities: [70, 75],
+  },
 };
 
 export default nextConfig;
